d21e8f77ff0210175bcd31b27c8294e0eee8fab4
Carlos Rogner <carlosrogner@gmail.com>
2021-04-20 00:18:57 -0300

    API base criada

--- src/Routes/schedule.route.js
+++ src/Routes/schedule.route.js
@@ -7,4 +7,5 @@ const routes = express.Router();
 routes.get("/schedule", ScheduleController.index);
 routes.post("/todo", ScheduleController = require(ScheduleController.store))
 
+
 module.exports = routes;
